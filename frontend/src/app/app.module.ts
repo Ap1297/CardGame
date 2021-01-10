1f20a4c241f8b646cc324b62687f042d6e2da3f7
am.panchal97@gmail.com <40174133+Ap1297@users.noreply.github.com>
2021-01-10 12:10:38 +0530

    Frontend commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { SharedComponent } from './shared/shared.component';
+import { UserComponent } from './user/user.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './user/reset-password/reset-password.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    SharedComponent,
+    UserComponent,
+    UserProfileComponent,
+    ForgotPasswordComponent,
+    ResetPasswordComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/shared/user.model.ts
@@ -0,0 +1,10 @@
+export class User {
+    fullName: string;
+    email:string;
+    password:string;
+    ipaddress:string;
+    mobileNo: string;
+    userType: string;
+}
+
+
